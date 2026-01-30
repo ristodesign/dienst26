a7d74f94290b8007e8bbf17cc493c98017a8dda8
Shift <shift@laravelshift.com>
2026-01-30 20:29:23 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files easier, Shift defaulted them and merged your true customizations - where ENV variables may not be used.

--- config/services.php
+++ config/services.php
@@ -3,21 +3,22 @@
 return [
 
     /*
-  |--------------------------------------------------------------------------
-  | Third Party Services
-  |--------------------------------------------------------------------------
-  |
-  | This file is for storing the credentials for third party services such
-  | as Mailgun, Postmark, AWS and more. This file provides the de facto
-  | location for this type of information, allowing packages to have
-  | a conventional file to locate the various service credentials.
-  |
-  */
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
     ],
 
     'postmark' => [
@@ -43,6 +44,7 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+
     'zoom' => [
         'account_id' => env('ACCOUNT_ID'),
         'client_id' => env('CLIENT_ID'),
